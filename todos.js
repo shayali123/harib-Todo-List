ec763c38e0b680e5272a9b0ac5bdf0bd846a4469
shayali123 <160710648+shayali123@users.noreply.github.com>
2024-04-13 10:40:00 +0500

    Push My Todos List To GitHub

---
+++ todos.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todo = await inquirer.prompt([
+        {
+            name: "question",
+            type: "input",
+            message: "what you want to add in your todos?"
+        },
+        {
+            name: "Question",
+            type: "confirm",
+            message: "do you want to add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(todo.question);
+    condition = (todo.Question);
+    console.log(todos);
+}
